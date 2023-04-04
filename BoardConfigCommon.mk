80f7ec8c163c68ae664c96cffd305a6fa69d1d0c
Michael Bestas <mkbestas@gmail.com>
2023-04-04 22:13:57 +0300

    Do not build disabled vbmeta
    Change-Id: Iac7e27d87cc85dac36a9dd2b9384079e105a22da

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,6 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
