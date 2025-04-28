0338abe0532950e4f8b19fa3262228c8be476679
Chirayu Desai <chirayudesai1@gmail.com>
2025-04-29 04:53:05 +0530

    Temporarily remove charging control support
    Issue: calyxos#3203
    Issue: calyxos#3204
    Change-Id: Iebc244632dcdb9fe8b270ca97b1df627cac1dda9

--- common.mk
+++ common.mk
@@ -199,10 +199,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
 # Media
 PRODUCT_PACKAGES += \
     libOmxCore \
